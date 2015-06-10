0767c1f412b6751da2e1d74110799eb28c5aad98
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-06-10 22:30:20 +0200

    Revert "Store all local contacts in only one account."
    This reverts commit fc1a70a5d043822c7f1f0dda6617d9f5c741543a.

--- src/com/android/contacts/common/model/account/AccountType.java
+++ src/com/android/contacts/common/model/account/AccountType.java
@@ -59,6 +59,12 @@ public abstract class AccountType {
     private static final String TAG = "AccountType";
 
     /**
+     * Local phone-storage account
+     * @hide
+     */
+    public static final String LOCAL_ACCOUNT = "phone-local";
+
+    /**
      * The {@link RawContacts#ACCOUNT_TYPE} these constraints apply to.
      */
     public String accountType = null;
@@ -292,9 +298,6 @@ public abstract class AccountType {
     public CharSequence getDisplayLabel(Context context) {
         CharSequence label = null;
         updateAuthDescriptions(context);
-        if (PhoneAccountType.ACCOUNT_TYPE.equals(accountType)) {
-            return context.getResources().getString(R.string.local_storage_account);
-        }
         if (mTypeToAuthDescription.containsKey(accountType)) {
             try {
                 AuthenticatorDescription desc = mTypeToAuthDescription.get(accountType);
